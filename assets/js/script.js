ebf1f5b660f4af1b103d4354ceda1225eb7c6676
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-06-30 22:52:29 +0100

    Encapsulating URLs in functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+  /* Code setting the API_KEY */
   const apiKeyInput = document.getElementById("api-key");
   // Fetch API Key from local storage if empty
   if (apiKeyInput.value.trim() === "") {
     apiKeyInput.value = localStorage.getItem("OpenWeatherApiKey") || "";
   }
+
   let API_KEY = apiKeyInput.value.trim() || "YOUR_API_KEY_HERE";
-
-  // First, get the latitude and longitude for the city
-//   const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
-
-  // Call getWeather API when the button is clicked
-//   const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
 
   apiKeyInput.addEventListener("input", () => {
     localStorage.setItem("OpenWeatherApiKey", apiKeyInput.value.trim());
     API_KEY = apiKeyInput.value.trim() || "YOUR_API_KEY_HERE";
   });
+
+  /* Code fetching the Latitude and Longitude */
+  // First, get the latitude and longitude for the city
+  function getLocation() {
+    const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+  }
+
+  /* Code fetching the weather data */
+  // Call getWeather API when the button is clicked
+  function getWeather() {
+    const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  }
+
 });
