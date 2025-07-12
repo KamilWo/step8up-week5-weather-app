d7094afd12ddd05c98d2ba2459b305bfabe33729
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-07-12 15:38:55 +0100

    First static version

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,7 +1,29 @@
-const API_KEY = "***";
+document.addEventListener("DOMContentLoaded", (event) => {
+  const glassRect = document.querySelector('.glass-rectangle');
+  const dashboardContainer = document.querySelector('.dashboard-container');
 
-// First, get the latitude and longitude for the city
-const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+  const API_KEY = "***";
 
-// Call getWeather API when the button is clicked
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  // First, get the latitude and longitude for the city
+  const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`;
+
+  // Call getWeather API when the button is clicked
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+
+  function updateBackground(weatherCondition) {
+
+    glassRect.classList.remove('sunny', 'cloudy', 'partly-cloudy', 'rainy', 'snowy', 'night-sky', 'night-rainy-sky', 'night-cloudy-sky');
+    if (weatherCondition.includes('clear') && isDay) {
+      glassRect.classList.add('sunny');
+    } else if (weatherCondition.includes('cloud')) {
+      glassRect.classList.add('cloudy');
+    } else if (weatherCondition.includes('rain')) {
+      glassRect.classList.add('rainy');
+    }
+  }
+
+  let visitCount = localStorage.getItem('websiteVisitCount') || 0;
+  visitCount++;
+  localStorage.setItem('websiteVisitCount', visitCount);
+  document.getElementById('visitCount').textContent = visitCount;
+});
