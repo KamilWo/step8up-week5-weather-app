382e75622e4e0acdfd892e239748f4926f76b5a4
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-07-12 14:18:50 +0100

    Initial website

---
+++ step8up-week5-weather-app.666000a4.js
@@ -0,0 +1,2 @@
+let API_KEY="***",geoUrl=`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=***`,weatherUrl=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=***&units=metric`;
+//# sourceMappingURL=step8up-week5-weather-app.666000a4.js.map
