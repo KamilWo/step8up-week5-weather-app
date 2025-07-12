4e5c19b47fccc9df66cee738789269afb2e0e8ff
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-07-12 15:48:18 +0100

    First static website

---
+++ weather-dashboard.d93eeb88.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",t=>{document.querySelector(".glass-rectangle"),document.querySelector(".dashboard-container"),city,lat,lon;let e=localStorage.getItem("websiteVisitCount")||0;e++,localStorage.setItem("websiteVisitCount",e),document.getElementById("visitCount").textContent=e});
+//# sourceMappingURL=weather-dashboard.d93eeb88.js.map
