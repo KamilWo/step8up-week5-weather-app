049faf009cff51ffecdd8baca6b953a552a3299b
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2025-07-13 18:58:10 +0100

    Clock added, about and contact pages

---
+++ contact.ac7ee8ef.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",e=>{let t=document.getElementById("current-date-time");function n(){let e=new Date,n=e.toLocaleDateString("en-GB",{month:"long",day:"numeric",year:"numeric"}),i=e.toLocaleTimeString("en-GB",{hour:"2-digit",minute:"2-digit",second:"2-digit",hour24:!0});t&&(t.textContent=`${n} | ${i}`)}n(),setInterval(n,1e3)});
+//# sourceMappingURL=weather-dashboard.ac7ee8ef.js.map
